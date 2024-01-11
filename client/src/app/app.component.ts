c1520d04fa8684ac7684377276fb80044f5dc5b9
jamshaidazeem <jamshaidazeem@gmail.com>
2024-01-11 15:50:56 +0500

    client: chart.js package added, interval view added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,8 +7,21 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'client';
+  intervals = [
+    { label: '1s', value: 1 },
+    { label: '5s', value: 5 },
+    { label: '10s', value: 10 },
+    { label: '30s', value: 30 },
+    { label: '1m', value: 60 },
+  ];
+
+  selectedInterval = this.intervals[0].value;
+
+  onUpdateInterval = (intervalValue: number) => {
+    this.selectedInterval = intervalValue;
+  };
 }
